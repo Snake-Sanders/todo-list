dee4986369a62973ee21a253f1b9c27b7f7369d8
Snake Sanders <elmarian22@gmail.com>
2012-04-25 12:29:09 +0200

    list-task Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '5bf2127d0ed93f479f937bb18d404061c5cb87f5a45b4ace2c9155b7c1f7703cb43638f7d648f92d686784093f20b14a54dd024e44444fcfa758fa941f4f1fed'
